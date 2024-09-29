c33da5265fefd18041eed43f6fe7d65d1b8ab293
kukushka1370 <102031292+shheeeeshka@users.noreply.github.com>
2024-09-30 00:39:11 +0300

    fjoeif

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
-export const API_URL = "http://192.168.0.103:5080";
-export const SOCKET_URL = "http://192.168.0.103:5070";
+export const API_URL = "https://barter-node.onrender.com";
+// export const API_URL = "http://192.168.0.103:5080";
+// export const SOCKET_URL = "http://192.168.0.103:5070";
